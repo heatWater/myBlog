3f325cb4eaa2f107a2d26a50711f9316738005f0
heatWater <oytx_kong@163.com>
2021-12-06 11:57:29 +0800

    commit

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,24 @@
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '部署文档', link: '/pages/c5f2a9/', items: [
+        {text: 'V3', link: '/pages/c5f2a9/', items: [
+            {text: '部署流程', link: '/pages/c5f2a9/'}
+        ]},
+        {text: 'V4', link: '/pages/03eb7c/', items: [
+            {text: 'mqtt部署问题', link: '/pages/03eb7c/'}
+        ]},
+    ],
+  },
+  {
+    text: '数据库', items: [
+        {text: 'mysql', link: '/pages/545948/'}
+    ]
+  },
+  {
+      text: '开发', items: [
+          {text: 'SpringBoot依赖导入问题', link: '/pages/0eeddd/'}
+      ]
+  }
+  
+]
