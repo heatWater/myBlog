1d25ee8bd0c92436f0ef953267831b949ba4549b
heatWater <oytx_kong@163.com>
2021-12-07 09:35:16 +0800

    init2

--- deploy.sh
+++ deploy.sh
@@ -3,51 +3,23 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-initDist(){
-  echo $1 > base.js
-  npm run build
-  cd docs/.vuepress/dist
-}
+# 生成静态文件
+npm run docs:build
 
-initDist "module.exports = '/vuepress-theme-vdoing-doc/'"
-echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
+# 进入生成的文件夹
+cd docs/.vuepress/dist
 
-# deploy to github
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing-doc.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing-doc.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
-fi
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
 
-initGit(){
-  git init
-  git add -A
-  git commit -m "${msg}"
-}
-initGit
-git push -f $githubUrl master:gh-pages # 推送到github
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:heatWater/myBlog.git master
 
 cd -
-rm -rf docs/.vuepress/dist
-
-
-# deploy to coding
-# initDist "module.exports = '/'"
-# echo 'doc.xugaoyi.com' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true
-#   codingUrl=git@e.coding.net:xgy/vdoing-doc/vdoing-doc.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/vdoing-doc/vdoing-doc.git
-# fi
-
-# initGit
-# git push -f $codingUrl master # 推送到coding
-
-# cd -
-# rm -rf docs/.vuepress/dist
